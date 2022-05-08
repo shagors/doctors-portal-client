7918de024e8c225cb08919b8793a097d148911ea
shagors <shajjadshagor@gmail.com>
2022-05-09 03:19:48 +0600

    daisyui library thems setup and prsonalization

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="">
       <h1>test case</h1>
+      <button class="btn btn-primary">Button</button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,19 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [{
+        doctortheme: {
+          primary: "#0fcfec",
+          secondary: "#19D3AE",
+          accent: "#3a4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
