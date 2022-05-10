789db5e41e1bf59f9c0ebf025aacc0e90a6b743f
shagors <shajjadshagor@gmail.com>
2022-05-10 21:32:52 +0600

    add appointment component and show in appoinment page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const InfoCard = ({img, cardTitle, bgClass}) => {
     return (
         <div className={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
-            <figure className='pt-5 lg:pl-5 h-20 w-20'>
+            <figure className='pt-5 pl-5 h-20 w-20'>
                 <img src={img} alt="Album"/>
             </figure>
             <div className="card-body text-white">
