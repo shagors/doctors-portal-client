978fbbd941c85ac73beaf4f978114ca4e61e29a8
shagors <shajjadshagor@gmail.com>
2022-05-10 22:54:39 +0600

    add appointment slots and times in card

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,5 +1,6 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import Service from './Service';
 
 const AvailableAppointment = ({date}) => {
     const [services, setServices] = useState([]);
@@ -13,9 +14,12 @@ const AvailableAppointment = ({date}) => {
     return (
         <div>
             <h4 className='text-xl text-secondary text-center'>Available Appointment On {format(date, 'PP')}</h4>
-            <div>
+            <div className='grid lg:grid-cols-3 gap-10'>
                 {
-                    services.map(service => <p>{service.name}</p>)
+                    services.map(service => <Service
+                    key={service._id}
+                    service={service}
+                    ></Service>)
                 }
             </div>
         </div>
