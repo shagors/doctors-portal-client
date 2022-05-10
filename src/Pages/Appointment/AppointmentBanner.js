0f4fa65676144b4f6286866b3e6563f2696d82b1
shagors <shajjadshagor@gmail.com>
2022-05-10 21:45:55 +0600

    add apointment banner on appointment page

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../assets/images/chair.png'
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
 
 const AppointmentBanner = () => {
+    const [date, setDate] = useState(new Date());
     return (
         <div class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='Dentist Chair'/>
                 <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <DayPicker
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                    ></DayPicker>
+                    <p>You Have Selected: {format(date, 'PP')}</p>
                 </div>
             </div>
         </div>
