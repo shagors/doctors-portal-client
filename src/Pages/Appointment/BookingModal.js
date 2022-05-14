60137b5789d519e5d6e85c964c0cbcbbef33c0fa
shagors <shajjadshagor@gmail.com>
2022-05-14 15:44:52 +0600

    add google signin option and react hook form install

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -19,16 +19,16 @@ const BookingModal = ({date, treatment, setTreatment}) => {
                 <label for="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                 <h3 className="font-bold text-lg text-secondary text-center">Booking For : {name}</h3>
                 <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 justify-items-center mt-3'>
-                    <input type="text" disabled value={format(date, 'PP')} class="input input-bordered w-full max-w-xs" />
-                    <select name='slot' class="select select-bordered w-full max-w-xs">
+                    <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
+                    <select name='slot' className="select select-bordered w-full max-w-xs">
                         {
                             slots.map(slot => <option value={slot}>{slot}</option>)
                         }
                     </select>
-                    <input type="text" name='name' placeholder="Your Name" class="input input-bordered w-full max-w-xs" />
-                    <input type="email" name='email' placeholder="Email Address" class="input input-bordered w-full max-w-xs" />
-                    <input type="number" name='phone' placeholder="Phone" class="input input-bordered w-full max-w-xs" />
-                    <input type="submit" value="Submit" class="btn btn-secondary w-full max-w-xs" />
+                    <input type="text" name='name' placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
+                    <input type="email" name='email' placeholder="Email Address" className="input input-bordered w-full max-w-xs" />
+                    <input type="number" name='phone' placeholder="Phone" className="input input-bordered w-full max-w-xs" />
+                    <input type="submit" value="Submit" className="btn btn-secondary w-full max-w-xs" />
                 </form>
             </div>
             </div>
