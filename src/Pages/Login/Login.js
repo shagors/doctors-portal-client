4eef667943d02c7974ac1c972c26a84e67c8a565
shagors <shajjadshagor@gmail.com>
2022-05-14 16:02:39 +0600

    set from input field

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import auth from '../../firebase.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from "react-hook-form";
+
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    
+    const onSubmit = data => {
+        console.log(data)
+    };
 
     if(user){
         console.log(user);
@@ -14,7 +21,29 @@ const Login = () => {
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="text-center text-3xl font-bold">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
 
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="Your email" 
+                            className="input input-bordered w-full max-w-xs" 
+                             {...register("email", { pattern: {
+                                 value: /[A-Za-z]{3}/,
+                                 message: 'error message'
+                             } })}/>
+                            <label className="label">
+                                <span className="label-text-alt">Alt label</span>
+                            </label>
+                        </div>
+
+                        <input/>
+                        {errors.firstName?.type === 'required' && "First name is required"}
+                        <input {...register("lastName", { required: true })} />
+                        {errors.lastName && "Last name is required"}
+                        <input type="submit" />
+                    </form>
                     <div className="divider">OR</div>
                     <button 
                     onClick={() => signInWithGoogle()} className="btn btn-outline">Continue With Google</button>
