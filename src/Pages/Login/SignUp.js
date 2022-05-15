f178ef030f3fa229231748a573aa4430212ef2c2
shagors <shajjadshagor@gmail.com>
2022-05-15 10:57:06 +0600

    add sign in and signout options add

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import auth from '../../firebase.init';
-import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfile } from 'react-firebase-hooks/auth';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -14,25 +14,31 @@ const SignUp = () => {
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const navigate = useNavigate();
 
     let signInError;
 
-    if(loading || gLoading){
+    if(loading || gLoading || updating){
         return <Loading></Loading>
     }
 
-    if(error || gError){
-        signInError = <p className='text-red-500'>{error?.message || gError?.message}</p>
+    if(error || gError || updateError){
+        signInError = <p className='text-red-500'>{error?.message || gError?.message || updateError?.message}</p>
     }
-
-    const onSubmit = data => {
-        console.log(data)
-        createUserWithEmailAndPassword(data.email, data.password);
-    };
-
+    
     if(user || gUser){
         console.log(user || gUser);
     }
+
+    const onSubmit = async data => {
+        await createUserWithEmailAndPassword(data.email, data.password);
+        await updateProfile({displayName: data?.name})
+        console.log('updated');
+        navigate('/appointment');
+    };
+
 
     return (
         <div className='flex h-screen justify-center items-center'>
