71194946d8307b70f215bdb333808b5c43ec5d02
shagors <shajjadshagor@gmail.com>
2022-05-17 00:03:14 +0600

    show all users on table

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import UserRow from './UserRow';
 
 const Users = () => {
-    const {data: users, isLoading} = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()))
-    if(isLoading){
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers:{
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    if (isLoading) {
         return <Loading></Loading>
     }
-    
     return (
         <div>
-            <h2 className="text-xl text-center">All Users: {users?.length}</h2>
-            <div className="overflow-x-auto">
-                <table className="table w-full">
+            <h2 className="text-2xl">All Users: {users.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
                     <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Job</th>
-                        <th>Favorite Color</th>
-                    </tr>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <th>1</th>
-                        <td>Cy Ganderton</td>
-                        <td>Quality Control Specialist</td>
-                        <td>Blue</td>
-                    </tr>
+                       {
+                           users?.map(user=><UserRow
+                           key={user._id}
+                           user={user}
+                           refetch={refetch}
+                           ></UserRow>)
+                       }
                     </tbody>
                 </table>
             </div>
