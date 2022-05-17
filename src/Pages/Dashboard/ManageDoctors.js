b70993a14947972270275a8f8862ca0f352394e3
shagors <shajjadshagor@gmail.com>
2022-05-17 22:40:24 +0600

    doctor delete from doctors page and confirm and validate it

--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
-    const {data: doctors, isLoading} = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
+    const {data: doctors, isLoading, refetch} = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
@@ -16,6 +17,29 @@ const ManageDoctors = () => {
     return (
         <div>
             <h2 className='text-2xl text-center'>Doctors List: {doctors?.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Avatar</th>
+                        <th>Name</th>
+                        <th>Specialty</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow
+                            key={doctor._id}
+                            doctor={doctor}
+                            index={index}
+                            refetch={refetch}
+                            ></DoctorRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
