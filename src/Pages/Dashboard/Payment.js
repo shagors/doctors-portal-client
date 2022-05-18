29ebb57ac3a3cfdd42d2124cefb6a9ac5f8ac5f4
shagors <shajjadshagor@gmail.com>
2022-05-18 13:45:23 +0600

    card add and check validation and card access

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import {loadStripe} from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+
+
+const stripePromise = loadStripe('pk_test_51L0fnhKA5GQnus2VzJO9R4QXKvEBIazsNhBj0EzZusUKAtXDjVXLPbsAdNqjSgf1zrbb8VrvbpQvIfTsJhtwixZ600iGxiMEC8');
 
 const Payment = () => {
     const {id} = useParams();
@@ -30,7 +36,11 @@ const Payment = () => {
             </div>
             <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
             <div className="card-body">
-
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm
+                    appointment={appointment}
+                    ></CheckoutForm>
+                </Elements>
             </div>
             </div>
         </div>
